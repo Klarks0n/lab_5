9e3304619d9df245b9e02ebe677f836672d6a0e9
Klarks0n <165905750+Klarks0n@users.noreply.github.com>
2024-05-04 12:36:44 +0200

    Add files via upload

---
+++ company/implementations/EmailMessenger.java
@@ -0,0 +1,8 @@
+import company.interfaces.Messenger;
+
+public class EmailMessenger implements Messenger {
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
